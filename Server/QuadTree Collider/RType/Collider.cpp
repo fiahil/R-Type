0f39fd3a47d2d81933726c0c82ff1c75781798fc
ossato-m <ossato_m@epitech.eu>
2013-01-09 17:09:40 +0100

    QuadTree & Collider testing.

--- Server/QuadTree Collider/RType/Collider.cpp
+++ Server/QuadTree Collider/RType/Collider.cpp
@@ -19,16 +19,53 @@ bool	Collider::collide(std::list<IEntity *>& items, IEntity *object)
 
 		if ((*it)->getXUp() < object->getXUp() &&  (*it)->getXUp() > object->getXDown() &&
 			(*it)->getYLeft() > object->getYLeft() &&  (*it)->getYLeft() < object->getYRight())
-				hasCollide = true;
+				{
+					hasCollide = true;
+					continue ;
+				}
 		if ((*it)->getXUp() < object->getXUp() &&  (*it)->getXUp() > object->getXDown() &&
 			(*it)->getYRight() > object->getYLeft() &&  (*it)->getYRight() < object->getYRight())
-				hasCollide = true;
+				{
+					hasCollide = true;
+					continue ;
+				}
 		if ((*it)->getXDown() < object->getXUp() &&  (*it)->getXDown() > object->getXDown() &&
 			(*it)->getYLeft() > object->getYLeft() &&  (*it)->getYLeft() < object->getYRight())
-				hasCollide = true;
+				{
+					hasCollide = true;
+					continue ;
+				}
 		if ((*it)->getXDown() < object->getXUp() &&  (*it)->getXDown() > object->getXDown() &&
 			(*it)->getYRight() > object->getYLeft() &&  (*it)->getYRight() < object->getYRight())
-				hasCollide = true;
+				{
+					hasCollide = true;
+					continue ;
+				}
+
+		if (object->getXUp() < (*it)->getXUp() &&  object->getXUp() > (*it)->getXDown() &&
+			object->getYLeft() > (*it)->getYLeft() &&  object->getYLeft() < (*it)->getYRight())
+				{
+					hasCollide = true;
+					continue ;
+				}
+		if (object->getXUp() < (*it)->getXUp() &&  object->getXUp() > (*it)->getXDown() &&
+			object->getYRight() > (*it)->getYLeft() &&  object->getYRight() < (*it)->getYRight())
+				{
+					hasCollide = true;
+					continue ;
+				}
+		if (object->getXDown() < (*it)->getXUp() &&  object->getXDown() > (*it)->getXDown() &&
+			object->getYLeft() > (*it)->getYLeft() &&  object->getYLeft() < (*it)->getYRight())
+				{
+					hasCollide = true;
+					continue ;
+				}
+		if (object->getXDown() < (*it)->getXUp() &&  object->getXDown() > (*it)->getXDown() &&
+			object->getYRight() > (*it)->getYLeft() &&  object->getYRight() < (*it)->getYRight())
+				{
+					hasCollide = true;
+					continue ;
+				}
 	}
 	return hasCollide;
 }

8198a69f044351a0752e5d8aefc19d12bafa3f3b
fourne_r <fourne_r@epitech.eu>
2013-01-12 14:44:31 +0100

    [Server] Adding IGameMod

---
+++ Server/Gameplay/include/IGameMod.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "IEntity.h"
+
+class IGameMod {
+public:
+  virtual void onCollision(IEntity*,IEntity*) = 0;
+  virtual void onEntityDestruction(IEntity*) = 0;
+  virtual void onFire(IEntity*) = 0;
+  virtual void onLevelStart() = 0;
+  virtual void onLevelEnd() = 0;
+  virtual void onModulePlug(IEntity*) = 0;
+  virtual void onModuleUnplug(IEntity*) = 0;
+  virtual void onNewEntity(IEntity*) = 0;
+  virtual void onPickUp(IEntity*) = 0;
+  virtual void onScreenIn(IEntity*) = 0;
+  virtual void onScreenOut(IEntity*) = 0;
+  virtual void onScriptBegin(IEntity*) = 0;
+  virtual void onScriptEnd(IEntity*) = 0;
+  virtual void onUltimate(IEntity*) = 0;
+};

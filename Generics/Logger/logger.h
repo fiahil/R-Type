282f37b0769b13fef1b35584415cea4e81bf02c4
ossato-m <ossato_m@epitech.eu>
2013-01-12 21:28:21 +0100

    [Logger] Refactorization of the logger

--- Generics/Logger/logger.h
+++ Generics/Logger/logger.h
@@ -13,25 +13,6 @@ public:
 	std::string		getLabel() const;
 };
 
-Logger::Logger(std::ostream& stream, std::string const& label) : oss_(stream), label_(label)
-{
-		
-}
-
-Logger::~Logger()
-{
-}
-
-std::ostream&	Logger::getStream()
-{
-	return this->oss_;
-}
-
-std::string		Logger::getLabel() const
-{
-	return this->label_;
-}
-
 template <class T>
 std::ostream& operator<<(Logger& logger, T o)
 {

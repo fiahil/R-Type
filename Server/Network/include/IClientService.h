2eddb5d98465e5af2da0ac83f911aeb4b9d5f901
Fiahil <fiahil@gmail.com>
2013-01-12 14:02:17 +0100

    [Patch] tree

---
+++ Server/Network/include/IClientService.h
@@ -0,0 +1,19 @@
+//
+// IClientService.hpp for R-Type in /home/teisse_a//Documents/Tek3/RType/R-Type/Server/NetWork/include
+// 
+// Made by alexandre teisseire
+// Login   <teisse_a@epitech.net>
+// 
+// Started on  Wed Jan  9 11:24:52 2013 alexandre teisseire
+// Last update Wed Jan  9 11:26:13 2013 alexandre teisseire
+//
+
+#pragma once
+
+#include "IService.h"
+
+class		IClientService : public IService
+{
+  virtual ~IClientService() {}
+  virtual void	connectTo() = 0;
+};

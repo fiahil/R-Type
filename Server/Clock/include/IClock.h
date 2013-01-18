08d94f067306f1c8254af50674a8fd0221c2bbb2
TaZ <slick.260@gmail.com>
2013-01-18 17:06:48 +0100

    [SERVER] -> Fix Clock : Abstraction Unix & Windows. Need to Test Windows

---
+++ Server/Clock/include/IClock.h
@@ -0,0 +1,22 @@
+/*
+** IClock.h for RType in /home/teisse_a//Documents/Tek3/RType/R-Type/Server/Clock/timer/src
+** 
+** Made by alexandre teisseire
+** Login   <teisse_a@epitech.net>
+** 
+** Started on  Fri Jan 18 15:49:57 2013 alexandre teisseire
+** Last update Fri Jan 18 16:22:32 2013 alexandre teisseire
+*/
+
+#ifndef _CLOCK_H_
+#define _CLOCK_H_
+
+class	IClock
+{
+public:
+  virtual ~IClock() {}
+  virtual void	reset() = 0;
+  virtual float getElapsedTime() = 0;
+};
+
+#endif
---
+++ Server/Clock/include/UnixGameClock.h
@@ -0,0 +1,31 @@
+/*
+** UnixGameClock.h for RType in /home/teisse_a//Documents/Tek3/RType/R-Type/Server/Clock/timer/src
+** 
+** Made by alexandre teisseire
+** Login   <teisse_a@epitech.net>
+** 
+** Started on  Fri Jan 18 16:00:13 2013 alexandre teisseire
+** Last update Fri Jan 18 16:59:37 2013 alexandre teisseire
+*/
+
+#ifndef _UNIXGAMECLOCK_H_
+#define _UNIXGAMECLOCK_H_
+
+#include	<time.h>
+#include	<sys/time.h>
+#include	"IClock.h"
+
+class	UnixGameClock : public IClock
+{
+ private:
+  struct timeval _startMark;
+  
+ public:
+  UnixGameClock();
+  virtual ~UnixGameClock();
+  
+  virtual void	reset();
+  virtual float getElapsedTime();
+};
+
+#endif

916ec2bec00dce0efd6e7719d8d1720dfdc9bb10
TaZ <slick.260@gmail.com>
2013-01-18 15:28:08 +0100

    [SERVER] -> add d'une clock basique

---
+++ Server/Clock/include/IClock.hpp
@@ -0,0 +1,22 @@
+//
+// Iclock.hpp for RType in /home/teisse_a//Documents/Tek3/RType/R-Type/Server/Clock
+// 
+// Made by alexandre teisseire
+// Login   <teisse_a@epitech.net>
+// 
+// Started on  Fri Jan 18 14:36:10 2013 alexandre teisseire
+// Last update Fri Jan 18 15:15:58 2013 alexandre teisseire
+//
+
+#ifndef	__ICLOCK_HPP__
+#define	__ICLOCK_HPP__
+
+class	IClock
+{
+public:
+  ~IClock() {}
+  virtual void	update() = 0;
+  virtual float getElapsedTime() const = 0;
+};
+
+#endif

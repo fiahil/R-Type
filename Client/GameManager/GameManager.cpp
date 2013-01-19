244bc26fcf7734bc036cbb94b6b996f02feb65bd
ossato-m <ossato_m@epitech.eu>
2013-01-19 17:29:17 +0100

    [Client] Commit for merging with TaZ (useless)

--- Client/GameManager/GameManager.cpp
+++ Client/GameManager/GameManager.cpp
@@ -19,7 +19,7 @@ void	GameManager::treatAction(ICommand *action)
 
 	if (move)
 	{
-			//stack_.top->treatMove(move);
+			stack_.top->treatMove(move);
 			return ;
 	}
 	Command<Death> * death = dynamic_cast<Command<Death> *>(action);
@@ -46,3 +46,30 @@ ICommand*	GameManager::getAction()
 {
 	return net_->getAction();
 }
+/*
+void	Stage::treatMove(Command<Move> *cmd)
+{
+	struct Move deplace = cmd->getT();
+	this->components.at(deplace.id).move(deplace.x, deplace.y);
+}
+
+void	Stage::treatDeath(Command<Death> *cmd)
+{
+	struct Death dead = cmd->getT();
+	this->components.at(dead.id).death();
+}
+
+void	Stage::treatDeath(Command<Fire> *cmd)
+{
+	struct Fire shoot = cmd->getT();
+	this->components.at(shoot.id).fire(shoot.x, shoot.y);
+}
+#include	"GameComponent.h"
+void	Stage::treatNewEntity(Command<NewEntity> *cmd)
+{
+	struct NewEntity life = cmd->getT();
+	ISpriteSheet *sprite = new sfSpriteSheet(life.filename, life.anim, life.frame);
+	ISpriteSheet *death = new sfSpriteSheet("..\\Resources\\Sprite\\little_death.gif", 1, 6);
+	this->components.at(life.id) = new GameComponent(sprite, death);
+}
+*/
